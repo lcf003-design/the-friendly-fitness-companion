4e5d7571a1809f4767074fe24471fd1a6df5cc96
larry fields <lcf003@icloud.com>
2026-05-06 19:35:58 +0100

    Fix CODE_SIGN_ENTITLEMENTS path

--- add_entitlements.rb
+++ add_entitlements.rb
@@ -4,7 +4,7 @@ project_path = 'The Friendly Fitness Companion/The Friendly Fitness Companion.xc
 project = Xcodeproj::Project.open(project_path)
 target = project.targets.first
 
-entitlements_path = 'The Friendly Fitness Companion/The Friendly Fitness Companion.entitlements'
+entitlements_path = 'The Friendly Fitness Companion.entitlements'
 
 # Add the entitlement key to build settings
 target.build_configurations.each do |config|
